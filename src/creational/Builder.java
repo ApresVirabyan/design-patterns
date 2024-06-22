5022e6f52f29c666101c797f0051db03eea97594
apres <apres1987@mail.ru>
2024-06-22 12:51:17 +0400

    design patterns

---
+++ src/creational/Builder.java
@@ -0,0 +1,75 @@
+package creational;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * https://www.digitalocean.com/community/tutorials/builder-design-pattern-in-java
+ */
+
+
+public class Builder {
+    public static void main(String[] args) {
+        Computer computer = new Computer.ComputerBuilder("500 GB", "2 GB")
+                .setBluetoothEnabled(true)
+                .setGraphicsCardEnabled(true)
+                .build();
+    }
+}
+
+@Getter
+class Computer {
+
+    // required parameters
+    private String HDD;
+    private String RAM;
+
+    // optional parameters
+    private boolean isGraphicsCardEnabled;
+    private boolean isBluetoothEnabled;
+
+    Computer(ComputerBuilder builder) {
+        this.HDD = builder.HDD;
+        this.RAM = builder.RAM;
+        this.isGraphicsCardEnabled = builder.isGraphicsCardEnabled;
+        this.isBluetoothEnabled = builder.isBluetoothEnabled;
+    }
+
+
+    public static class ComputerBuilder {
+
+        // required parameters
+        private String HDD;
+        private String RAM;
+
+        // optional parameters
+        private boolean isGraphicsCardEnabled;
+        private boolean isBluetoothEnabled;
+
+        public ComputerBuilder(String HDD, String RAM) {
+            this.HDD = HDD;
+            this.RAM = RAM;
+        }
+
+        public ComputerBuilder setGraphicsCardEnabled(boolean isGraphicsCardEnabled) {
+            this.isGraphicsCardEnabled = isGraphicsCardEnabled;
+            return this;
+        }
+
+        public ComputerBuilder setBluetoothEnabled(boolean isBluetoothEnabled) {
+            this.isBluetoothEnabled = isBluetoothEnabled;
+            return this;
+        }
+
+        public Computer build() {
+            return new Computer(this);
+        }
+
+    }
+}
+
+
+
+
