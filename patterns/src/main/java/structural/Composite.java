59eb7d4036090a39c125abfd897f7c66fd3b8faa
VirabyanApres <apres1987@mail.ru>
2024-06-23 17:25:27 +0400

    composite decorator

--- patterns/src/main/java/structural/Composite.java
+++ patterns/src/main/java/structural/Composite.java
@@ -1,4 +1,67 @@
 package structural;
 
+import java.util.ArrayList;
+import java.util.List;
+
+/** Скомпонировать обьекты в структуры по типу дерева
+ * для группировки мелких компонентов в более крупные.
+ * используется например в GUI Java*/
 public class Composite {
+
+    public static void main(String[] args) {
+
+        Team team = new Team();
+
+        Programmer javaProgrammer = new JavaProgrammer();
+        Programmer javaProgrammer2 = new JavaProgrammer();
+        Programmer cppProgrammer = new CppProgrammer();
+
+        team.addProgrammer(javaProgrammer);
+        team.addProgrammer(javaProgrammer2);
+        team.addProgrammer(cppProgrammer);
+
+        team.createProject();
+
+    }
 }
+
+class Team{
+
+    private List<Programmer> programmers = new ArrayList<>();
+
+    void addProgrammer(Programmer programmer){
+         programmers.add(programmer);
+    }
+
+    void deleteProgrammers(Programmer programmer){
+        programmers.remove(programmer);
+    }
+
+    public void createProject(){
+        System.out.println("Creating project");
+        for (Programmer programmer : programmers) {
+            programmer.writeCode();
+        }
+    }
+}
+
+interface Programmer{
+    void writeCode();
+}
+
+class JavaProgrammer implements Programmer{
+
+    @Override
+    public void writeCode() {
+        System.out.println("Java Programmer write code...");
+    }
+}
+
+class CppProgrammer implements Programmer{
+    @Override
+    public void writeCode() {
+        System.out.println("C++ Programmer write code...");
+    }
+}
+
+
--- patterns/src/main/java/structural/Decorator.java
+++ patterns/src/main/java/structural/Decorator.java
@@ -1,4 +1,73 @@
 package structural;
 
+/** Динамическое добавление новых обязанностей обьекту
+ *  в качестве альтернативы наследованию. */
 public class Decorator {
+    public static void main(String[] args) {
+        Coder senior = new SeniorJavaCoder(new JavaCoder());
+        System.out.println(senior.makeJob());
+
+        Coder lead = new JavaTeamLead(new SeniorJavaCoder(new JavaCoder()));
+        System.out.println(lead.makeJob());
+    }
 }
+
+class CoderDecorator implements Coder{
+
+    private Coder coder;
+
+    public CoderDecorator(Coder coder) {
+        this.coder = coder;
+    }
+
+    @Override
+    public String makeJob() {
+        return coder.makeJob();
+    }
+}
+
+interface Coder{
+    String makeJob();
+}
+
+class JavaCoder implements Coder{
+    public String makeJob(){
+        return "Write Java code...";
+    }
+}
+
+class SeniorJavaCoder extends CoderDecorator{
+
+    public SeniorJavaCoder(Coder coder) {
+        super(coder);
+    }
+
+    public String makeCodeReview(){
+        return "Make code review...";
+    }
+
+    @Override
+    public String makeJob() {
+        return super.makeJob() + makeCodeReview();
+    }
+}
+
+class JavaTeamLead extends CoderDecorator{
+
+    public JavaTeamLead(Coder coder){
+        super(coder);
+    }
+
+    public String manageTeam(){
+        return "Manage team...";
+    }
+
+    @Override
+    public String makeJob() {
+        return super.makeJob() + manageTeam();
+    }
+
+}
+
+
+
